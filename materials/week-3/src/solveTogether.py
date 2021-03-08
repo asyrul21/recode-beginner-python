99298116a0137e230c42b4a83d879e0afd79be70
asyrul21 <asyrulhafetzy.21@gmail.com>
2021-03-08 21:48:17 +0800

    week 3 take home challenge solution

--- materials/week-3/src/solveTogether.py
+++ materials/week-3/src/solveTogether.py
@@ -1,2 +1,18 @@
+print("hello world!")
 # 1. Calculate area of triangle
+height = 10
+width = 7
+area = (height * width) / 2
+
+print("The area of the triangle of height " + str(height) + "m and width " + str(width) + "m is " + str(area))
+
 # 2. Calculate the volume of cone
+pi = 3.14159
+r = 5
+radiusSquared = r * r
+height = 10
+
+volumeOfCone = pi * radiusSquared * (height / 3)
+
+print("The Volume of the Cone is: " + str(volumeOfCone))
+
