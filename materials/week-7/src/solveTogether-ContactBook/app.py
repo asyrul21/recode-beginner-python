65db789655e54871a2926589a85254f3187efc83
asyrul21 <asyrulhafetzy.21@gmail.com>
2021-03-31 22:04:22 +0800

    week 6 fibo take home challenge

--- materials/week-7/src/solveTogether-ContactBook/app.py
+++ materials/week-7/src/solveTogether-ContactBook/app.py
@@ -26,10 +26,36 @@ while(True):
     userAction = int(userAction)
 
     if(userAction == 1):
-        ...
+        tableDisplay.display(data)
+
+        name = input("What is the name of the new contact?: ")
+        relation = input("What is the relationship of the new contact?: ")
+        home = input("What is the home contact number?: ")
+        mobile = input("What is the mobile contact number?: ")
+
+        newContact = {
+            "name": name,
+            "relation": relation,
+            "home": home,
+            "mobile": mobile
+        }
+
+        data.append(newContact)
+        jsonRW.save(data)
+
+        tableDisplay.display(data)
+        showInvalidOrSuccess("Added a new record.")
 
     elif(userAction == 2):
-        ...
+        name = input("What is the name to search for?: ")
+
+        searchResults = []
+        for item in data:
+            if(name.lower() in item["name"].lower()):
+                searchResults.append(item)
+
+        tableDisplay.display(searchResults)
+        showInvalidOrSuccess(str(len(searchResults)) + " result(s) found.")
 
     elif(userAction == 3):
         break
@@ -39,6 +65,3 @@ while(True):
         
 
 navDisplay.printExit()
-
-
-
